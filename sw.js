5f4c0c3dad5b792b3be0b733468c6b79255777d8
AlvaroDueChiappe <alvaro2chiappe@gmail.com>
2025-11-10 09:16:29 +0100

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,40 +1,22 @@
-// Nome della cache
-const CACHE_NAME = 'offerteveloci-v1';
-
-// File da mettere in cache
-const URLS_TO_CACHE = [
+const cacheName = 'offerteveloci-cache-v1';
+const filesToCache = [
   '/',
   '/index.html',
-  '/offline.html'
+  '/install-ios.html',
+  '/offline.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-// Installazione
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
 });
 
-// Attivazione
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request).then(response => response || caches.match('/offline.html')))
+    fetch(event.request).catch(() => caches.match('/offline.html'))
   );
 });
